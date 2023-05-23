6940b6e12cb354da528267a317982947cfeebe73
jameshskelton <jameshskelton@gmail.com>
2023-05-23 18:19:00 -0500

    init commit

--- Dockerfile
+++ Dockerfile
@@ -5,3 +5,11 @@ WORKDIR app
 RUN apt-get update && apt-get install git-all -y
 RUN pip install -r requirements.txt
 EXPOSE 7860
+
+FROM python:3.8.16-slim-buster
+RUN sudo apt-get update
+RUN sudo apt-get install git-lfs
+RUN git-lfs clone $hf_repo
+WORKDIR $name
+
+EXPOSE 7860
--- run_huggingface.sh
+++ run_huggingface.sh
@@ -1,21 +1,42 @@
-while getopts u:s:d:t: flag
+while getopts u:s:d:t:q:v:z flag
 do
     case "${flag}" in
         u) url=${OPTARG};;
         s) space_name=${OPTARG};;
         d) docker_username=${OPTARG};;
         t) tag=${OPTARG};;
+        q) template=${OPTARG};;
+        v) vers=${OPTARG};;
     esac
 done
+# TESTS
 # echo tag $url
 # echo space name $space_name
 # echo your docker username $docker_username
 # echo tag for container $tag
+# echo template for container $template
+# echo version for container $vers
+# export date=$(date +%m%B-%T)
+
 
 git-lfs clone $url
 mkdir app
 mv $space_name/* ./app/
 rm -r -f $space_name
-docker compose build --no-cache
-# pspace init ./ -t Jameshskelton/pspace-huggingface
 
+# option one: Dockerhub
+
+# docker compose build --no-cache
+# docker -t $docker_username/$name:$vers
+# docker push $docker_username/$name:$vers
+# pspace init ./ -t $template
+# bash run_huggingface.sh -u https://huggingface.co/spaces/suno/bark -s bark -d jameshskelton -t eh -q jameshskelton/bark -z test6_version
+
+# option two: GHCR
+
+git remote add origin git@github.com:$fullname/$space_name.git
+git init
+git add .
+git commit -m 'init commit'
+gh repo create $space_name --private --source=. --remote=upstream --push
+pspace init . -t $template
