d823d615f0aae4f60923b329414b29bb3c45d8cf
jameshskelton <jameshskelton@gmail.com>
2023-05-25 17:36:38 -0500

    init commit

--- run_huggingface.sh
+++ run_huggingface.sh
@@ -10,36 +10,38 @@ do
     esac
 done
 # TESTS
-# echo tag $url
-# echo space name $space_name
-# echo your docker username $docker_username
-# echo tag for container $tag
-# echo template for container $template
-# echo version for container $vers
-# export date=$(date +%m%B-%T)
+echo tag $url
+echo space name $space_name
+echo your docker username $docker_username
+echo tag for container $tag
+echo template for container $template
+echo version for container $vers
+export date=$(date +%m%B-%T)
 
 
 git-lfs clone $url
-# mkdir app
-# mv $space_name/* ./app/
-# rm -r -f $space_name
-# cd $space_name
 
-# option one: Dockerhub
-export HF_REPO=$url
-export HF_NAME=$space_name
-docker compose build --no-cache --build-arg HF_REPO --build-arg HF_NAME 
-docker tag pspace-huggingface-hf-app $docker_username/$space_name:v-$vers
+
+# option one: Local build 
+# export HF_REPO=$url
+# export HF_NAME=$space_name
+# docker compose build --no-cache --build-arg HF_REPO --build-arg HF_NAME 
+# docker tag pspace-huggingface-hf-app $docker_username/$space_name:v-$vers
 # docker push $docker_username/$space_name:v-$vers
 # pspace init ./ -t $template
-# bash run_huggingface.sh -u https://huggingface.co/spaces/suno/bark -s bark -d jameshskelton -t 0.02 -q jameshskelton/bark -z test6_version
+# pspace up
 
-# option two: GHCR
 
-# git remote add origin git@github.com:$fullname/$space_name.git
-# git init
-# git add .
-# git commit -m 'init commit'
-# gh repo create $space_name --private --source=. --push
-# sleep 10
-# pspace init . -t $template
+# bash run_huggingface.sh -u https://huggingface.co/spaces/suno/bark -s bark -d jameshskelton -t eh -q jameshskelton/bark -z test6_version
+# option two: Github actions build
+mkdir app
+mv $space_name/* ./app/
+rm -r -f $space_name
+git remote add origin git@github.com:$fullname/$space_name.git
+git init
+git add .
+git commit -m 'init commit'
+gh repo create $space_name --private --source=. --push
+sleep 10
+pspace init . -t $template
+pspace up
